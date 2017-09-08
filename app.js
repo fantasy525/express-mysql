d75cceb7c298ae4bb2bf9ff75a61b4e534a85914
fantasy525 <1056726491@qq.com>
2017-09-08 14:34:52 +0800

    增加socket.io支持websocket.js

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var compression = require('compression');//gzip压缩
+var compression = require('compression'); //gzip压缩
 var ejs = require('ejs');
+var cors = require('cors');
+
 // 加载路由控制
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -18,14 +20,15 @@ var app = express();
 // 定义EJS模板引擎和模板文件位置，也可以使用jade或其他模型引擎
 app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'ejs');
-app.engine('.html',ejs.__express);
-app.set('view engine','html')
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html')
 
 // uncomment after placing your favicon in /public
 // 定义icon图标
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //在所有的中间件之前开启gzip压缩
-app.use(compression());  
+app.use(compression());
+//app.use(cors());
 // 定义日志和输出级别
 app.use(logger('dev'));
 // 定义数据解析器
@@ -36,22 +39,23 @@ app.use(cookieParser());
 // 定义静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
 // 匹配路径和路由
-//app.use('/', index);
+app.use('/', index);
 //给静态资源设置虚拟的目录前缀
-app.use('/static',express.static(__dirname + '/public'))
+app.use('/static', express.static(__dirname + '/public'))
 app.use('/users', users);
 app.use('/api', api)
 
+
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
-});
+  });
 
 // error handler
 // 开发环境，500错误处理和错误堆栈跟踪
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
